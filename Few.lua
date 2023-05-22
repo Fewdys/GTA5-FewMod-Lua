6f7f2b467cf0858f05e9bf018edbf11e4aa981f7
Fewdy <dipzei.vibes@gmail.com>
2023-05-22 13:28:52 -0400

    Update

--- Few.lua
+++ Few.lua
@@ -9,13 +9,13 @@ util.require_natives(1663599433)
 local response = false
 local localversion = 1.3
 local localKs = false
-async_http.init("raw.githubusercontent.com", "/Fewdys/GTA5-FewMod-Lua/main/FewModVersion.lua", function(output)
+async_http.init("https://raw.githubusercontent.com", "/Fewdys/GTA5-FewMod-Lua/main/FewModVersion.lua", function(output)
     currentVer = tonumber(output)
     response = true
     if localversion ~= currentVer then
         util.toast("There is an update for FewMod available, use the Update Button to update it.")
         menu.action(menu.my_root(), "Update Script", {}, "This Should Only Appear If The Script Is Not Up To Date\nGrabs The Newest Version Of Script From\nLink: https://github.com/Fewdys/GTA5-FewMod-Lua", function()
-            async_http.init('raw.githubusercontent.com','/Fewdys/GTA5-FewMod-Lua/main/Few.lua',function(a)
+            async_http.init('https://raw.githubusercontent.com','/Fewdys/GTA5-FewMod-Lua/main/Few.lua', function(a)
                 util.yield_once()
                 local err = select(2,load(a))
                 if err then
