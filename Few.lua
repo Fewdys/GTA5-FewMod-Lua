ff86e22680f56f4a2b7799cd52ac9e898b752652
Fewdy <dipzei.vibes@gmail.com>
2023-05-22 09:34:06 -0400

    Updated

--- Few.lua
+++ Few.lua
@@ -7,7 +7,7 @@ util.require_natives(1676318796)
 util.require_natives(1663599433)
 
 local response = false
-local localversion = 1.0
+local localversion = 1.1
 local localKs = false
 async_http.init("raw.githubusercontent.com", "/Fewdys/GTA5-FewMod/main/FewModVersion.lua", function(output)
     currentVer = tonumber(output)
