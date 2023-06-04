41ee4d531a94e0fa7f8c02fe6cceb7097f89e787
Fewdy <dipzei.vibes@gmail.com>
2023-06-04 01:17:57 -0400

    1.61 - Fixed Clean On Script Stop

--- Few.lua
+++ Few.lua
@@ -16742,25 +16742,21 @@ GenerateFeatures()
 util.on_pre_stop(function()
     menu.trigger_commands("clearworld")
     util.toast("Cleaning...")
-end)
-
-util.on_stop(function()
     --Incase "clearworld" above doesn't work
     ---------------------------------------------------------------------------------------
     GRAPHICS.REMOVE_PARTICLE_FX_IN_RANGE(player_pos.x, player_pos.y, player_pos.z, 1000000)
     menu.trigger_commands("clearropes")
-    util.yield(150)
     menu.trigger_commands("clearpeds")
-    util.yield(150)
     menu.trigger_commands("clearveh")
-    util.yield(150)
     menu.trigger_commands("clearobj")
-    util.yield(150)
     local delped = delete_entities_by_range(entities.get_all_peds_as_handles(), 1000000, "PED")
     local delveh = delete_entities_by_range(entities.get_all_vehicles_as_handles(), 1000000, "VEHICLE")
     local delobj = delete_entities_by_range(entities.get_all_objects_as_handles(), 1000000, "OBJECT")
     clear_area(10000)
     ----------------------------------------------------------------------------------------
+end)
+
+util.on_stop(function()
     VEHICLE.SET_VEHICLE_GRAVITY(veh, true)
     VEHICLE.SET_VEHICLE_REDUCE_GRIP(veh, false)
     ENTITY.SET_ENTITY_COLLISION(veh, true, true);
